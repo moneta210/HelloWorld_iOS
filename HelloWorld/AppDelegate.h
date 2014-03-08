0127650aab3927dc5aaf225167f0bcb8cc8926da
moneta210 <moneta210@gmail.com>
2014-03-08 10:31:22 +0800

    First Commit
    HelloWorld iOS app

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Steve Zhang on 3/8/14.
+//  Copyright (c) 2014 Moneta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Steve Zhang on 3/8/14.
+//  Copyright (c) 2014 Moneta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
